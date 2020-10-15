6b7b30361c5d6ecdb66abf90912c132a56a365c0
rafaelcastanheira <rafael.castanheira97@gmail.com>
2020-10-15 20:28:58 +0100

    commit 18

--- src/flask/setup.py
+++ src/flask/setup.py
@@ -2,12 +2,10 @@ import os
 from flask import Flask
 from src.flask.endpoint import api
 
-ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 api.init_app(app)
 
 if __name__ == "__main__":
     # hosting API in the following host and port
-    context = ('local.crt', 'local.key')
-    app.run(host="https://rafiquicf.herokuapp.com", port=os.environ.get("PORT", 5000), debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=True)
