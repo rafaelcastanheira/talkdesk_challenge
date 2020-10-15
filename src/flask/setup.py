fe6d432315204a1af616a88b3e3ad5176be1c073
rafaelcastanheira <rafael.castanheira97@gmail.com>
2020-10-15 18:58:29 +0100

    twelveth commit

--- src/flask/setup.py
+++ src/flask/setup.py
@@ -6,4 +6,4 @@ api.init_app(app)
 
 if __name__ == "__main__":
     # hosting API in the following host and port
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
