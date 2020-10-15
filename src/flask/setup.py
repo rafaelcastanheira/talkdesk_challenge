8e56221bee47b5d11af574b05a042f347f23b782
rafaelcastanheira <rafael.castanheira97@gmail.com>
2020-10-15 18:28:23 +0100

    secon commit

--- src/flask/setup.py
+++ src/flask/setup.py
@@ -4,6 +4,4 @@ from src.flask.endpoint import api
 app = Flask(__name__)
 api.init_app(app)
 
-if __name__ == "__main__":
-    # hosting API in the following host and port
-    app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
